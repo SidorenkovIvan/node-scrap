3b213ae40dd539af3d6fbd9c7de30872ee85eb0e
papano <mavrin.yuriy@gmail.com>
2020-03-23 03:58:26 +0300

    Добавил словарь для товаров. См. вывод в data.json

--- scrap.js
+++ scrap.js
@@ -6,6 +6,7 @@ let fs = require("fs");
 let siteUrl = "https://tea4u.by";
 let results = [];
 let category = {};
+let product = {};
 
 function start() {
     needle.get(siteUrl, (err, res) => {
@@ -16,7 +17,7 @@ function start() {
             q.push(h);
             category[h] = $(el).text().replace(/[^А-Яа-я0-9,Ёё]/g, ' ').trim();
         });
-        console.log(category);
+        //console.log(category);
     });
 }
 
@@ -29,31 +30,49 @@ let q = tress(function(url, callback) {
         let $ = cheerio.load(res.body);
         // делаем results.push для данных о товаре
         // делаем q.push для ссылок на обработку
-        // товар
-        // если использовать здесь функцию-стрелку () => {} , то $(this) работать не будет! (this - эквивалент el в данном случае)
-        $(".product-thumb").each(function (i, el) {
-            let imgUrl = $(el).find(".image > a > img").attr("src");
-            let a = $(el).find(".caption > a");
-            let title = a.text();
-            let productUrl = a.attr("href");
-            let categoryURL = productUrl.substr(0, productUrl.lastIndexOf('/'));
-            let categoryTitle = category[categoryURL];
-            results.push([
-                imgUrl,
-                title,
-                productUrl,
-                categoryURL,
-                categoryTitle
-            ]);
-        });
-        // паджинатор
-        let pli = $("ul.pagination li");
-        if (pli.length >= 4 && !pli.eq(-1).hasClass("active")) {
-            q.push(pli.eq(-2).children("a").attr("href"));
+        // в товаре внутри
+        if ($("#content").attr("itemtype") === "https://schema.org/Product") {
+            let code = $("meta[itemprop='model']").attr("content");
+            console.log(code);
+            if (!("description" in product[code])) {
+                product[code].description = $("#tab-description > div").text();
+            }
+        }
+        else {
+            // товар
+            // если использовать здесь функцию-стрелку () => {} , то $(this) работать не будет! (this - эквивалент el в данном случае)
+            $(".product-thumb").each(function (i, el) {
+                let imgUrl = $(el).find(".image > a > img").attr("src");
+                let a = $(el).find(".caption > a");
+                let productTitle = a.text();
+                let productUrl = a.attr("href");
+                let categoryURL = productUrl.substr(0, productUrl.lastIndexOf('/'));
+                let categoryTitle = category[categoryURL];
+                results.push([
+                    imgUrl,
+                    productTitle,
+                    productUrl,
+                    categoryURL,
+                    categoryTitle
+                ]);
+                let productCode = $(el).find(".additional .code > span").text();
+                let p = {imgUrl, productTitle, productUrl, categoryURL};
+                if (!(productCode in product)) {
+                    product[productCode] = {prods: [p]};
+                } else {
+                    product[productCode].prods.push(p);
+                }
+                q.push(productUrl);
+            });
+            // паджинатор
+            let pli = $("ul.pagination li");
+            if (pli.length >= 4 && !pli.eq(-1).hasClass("active")) {
+                q.push(pli.eq(-2).children("a").attr("href"));
+            }
         }
         callback(); //вызываем callback в конце
     });
-});
+}, 10); // запускаем 10 параллельных потоков
 
 // эта функция выполнится, когда в очереди закончатся ссылки
 q.drain = function () {
@@ -68,7 +87,7 @@ q.drain = function () {
        stmt.finalize();
        dataBase.close();
     });
-    fs.writeFileSync('./data.json', JSON.stringify(results, null, 4));
+    fs.writeFileSync('./data.json', JSON.stringify(product, null, 4));
 };
 
 // добавляем в очередь ссылки на категории из меню
